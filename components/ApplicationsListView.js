6fa74dc86f70cc4431a8a9a8518acb19fb6efd90
hobbleabbas <shakeel@atlasfinance.org>
2021-12-20 13:41:36 -0500

    loading state for uni apps

--- components/ApplicationsListView.js
+++ components/ApplicationsListView.js
@@ -138,6 +138,24 @@ export const ApplicationCard = ({ application, setApplicationItem, setListView }
     )
 }
 
+export const ManageApplicationButton = ({ application, setApplicationItem }) => {
+
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <button
+            type="button"
+            onClick={() => {
+                setLoading(true)
+                setApplicationItem(application)
+            }}
+            className="hidden sm:block inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        >
+            { loading ? "Loading Application..." : <>Manage Application &rarr;</>};
+        </button>
+    )
+}
+
 export const SecondaryApplicationsView = () => {
 
     const [secondaryApplications, setSecondaryApplications] = useState([])
